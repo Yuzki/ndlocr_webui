183bb4530d9d45ef15d80fdeef6bd975c1a59cdb
Yuzki <Yuzki@users.noreply.github.com>
2023-06-16 14:25:49 +0900

    Corrected options

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -d --rm --name ocr_cli_runner -v /home/yzk/ndl/tmp:/root/ndl -i ocr-cli-py37:latest
+docker run --gpus all -d --rm --name ocr_cli_runner -p 7860:7860 -i ocr-cli-py37:latest
